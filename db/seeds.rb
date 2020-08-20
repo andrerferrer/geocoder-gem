7433fce312022abe80226221bb159ee6cf38ba95
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-20 13:34:09 -0300

    Improve the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "Create some Restaurants"
 
-Restaurant.create! name: "Amareleen", address: "Gloria"
-Restaurant.create! name: "Verdeen", address: "Gloria"
+Restaurant.create! name: "Amareleen", address: "Ladeira da Glória, 8 - Glória, Rio de Janeiro - RJ, 22211-120"
+Restaurant.create! name: "Verdeen", address: "Ladeira da Glória, 8 - Glória, Rio de Janeiro - RJ, 22211-120"
 
 puts "Let's drink a beer"
 puts %{
